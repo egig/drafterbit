255c8d8e4aa0e335c018810c13d26df8bfc8035d
egig <egigundari@gmail.com>
2015-07-04 01:31:58 +0700

    working on mobile layout

--- src/Drafterbit/Bundle/SystemBundle/Resources/public/js/layout.js
+++ src/Drafterbit/Bundle/SystemBundle/Resources/public/js/layout.js
@@ -2,10 +2,9 @@ $(function(){
 
     var alertScreenSize = function()
     {
-        $('#screen-size-trapper').remove();
         var wW = $(window).innerWidth();
         if(wW <= 800) {
-            $('body').append('<div id="screen-size-trapper" style="z-index:1050;background:white;position:fixed; top:0;bottom:0;left:0;right:0">Screen size not supported yet, sorry</div>');
+            $("#sticky-toolbar").trigger("sticky_kit:detach");
         }
     }
 
@@ -22,16 +21,20 @@ $(function(){
         handleFooter: function(){    
             var h = $('.page-wrapper').height();
             var hW = $(window).innerHeight();
+            var wW = $(window).innerWidth();
 
-            if(h+30 < hW) {
+            if(h+30 < hW && wW >= 746) {
                 $('.footer').addClass('navbar-fixed-bottom');
             }
         },
 
         handleStickyToolbar: function(){
             // stick sticky-toolbar
-            if($('#sticky-toolbar') && typeof $.fn.stick_in_parent != 'undefined') {
-                $('#sticky-toolbar').stick_in_parent({offset_top:50});
+            var wW = $(window).innerWidth();
+            if(wW >= 800) {
+                if($('#sticky-toolbar') && typeof $.fn.stick_in_parent != 'undefined') {
+                    $('#sticky-toolbar').stick_in_parent({offset_top:50});
+                }
             }
         },
 
@@ -44,8 +47,10 @@ $(function(){
                     leftOffset = $('.dt-off-canvas').css('left');
                     if (leftOffset === '0px') {
                         $('.dt-off-canvas').animate({left:-200}, 300);
+                        $('.page-wrapper').animate({left:0}, 300);
                     } else {
                         $('.dt-off-canvas').animate({left:0}, 300);
+                        $('.page-wrapper').animate({left:200}, 300);
                     }
                 }
             )
