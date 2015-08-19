e3af224d4bb50ab4d599db4d0214c6fa3afa69f6
egig <egigundari@gmail.com>
2015-08-20 00:17:30 +0700

    add feed test

--- src/Bundle/BlogBundle/Tests/Controller/PostControllerTest.php
+++ src/Bundle/BlogBundle/Tests/Controller/PostControllerTest.php
@@ -30,4 +30,12 @@ class PostControllerTest extends WebTestCase
         $crawler = $client->request('GET', $this->adminPath('blog/post/edit/new'));
         $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode() );
 	}
+
+	public function testFeedAction()
+	{
+		$client = static::createClient();
+        $crawler = $client->request('GET', 'feed.xml');
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode() );
+        $this->assertEquals('application/xml', $client->getResponse()->headers->get('Content-Type'));
+	}
 }
