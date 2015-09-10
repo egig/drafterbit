bdeb29a04cf04bcf84cad558266e86d359baeb57
egig <egigundari@gmail.com>
2015-09-10 11:43:21 +0700

    git split dashboard controller

---
+++ src/Bundle/SystemBundle/Controller/DashboardController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Drafterbit\Bundle\SystemBundle\Controller;
+
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+use Drafterbit\Bundle\SystemBundle\Entity\Panel;
+
+/**
+ * @Route("%admin%")
+ */
+class DashboardController extends Controller
+{
+	 /**
+     * @Route("/system/dashboard/edit/{id}", name="dt_system_dashboard_edit")
+     * @Template("SystemBundle:Panel:edit.html.twig")
+     */
+    public function dashboardEditAction($id, Request $request)
+    {
+        // get panel from database
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('SystemBundle:Panel');
+        $panel = $repo->find($id);
+
+        if(!$panel) {
+            // if panel not found by given id, we'll assume id is panel name
+            $panelType = $this->get('dashboard')->getPanel($id);
+
+            if(!$panelType) {
+                throw $this->createNotFoundException();
+            }
+
+            $panel = new Panel;
+            $panel->setUser($this->getUser());
+            $panel->setName($id);
+            $panel->setPosition('left');
+            $panel->setSequence(0);
+            $panel->setStatus(1);
+
+        } else {
+
+            //get panel from dashboard manager
+            $panelType = $this->get('dashboard')->getPanel($panel->getName());
+        }
+
+
+        $panelData = json_decode($panel->getContext());
+        $title = empty($panelData->title) ? $panelType->getName() : $panelData->title;
+        //we need this since its not root form
+
+        $form = $this->get('form.factory')->createNamedBuilder('panel')
+            ->add('title', 'text', ['data' => $title])
+            ->add('Save', 'submit')
+            ->getForm();
+            
+        $panelForm = $panelType->getForm($panelData);
+        if($panelForm) {
+            //we need this since its not root form
+            $panelForm->getConfig()->setAutoInitialize(false);    
+            $form->add($panelForm);
+        }
+
+
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            // @todo get data from the form
+            $data = $request->request->get('panel');
+            $context = isset($data['context']) ? $data['context'] : []; 
+            $context = array_merge($context, ['title' => $data['title']]);
+            $panel->setContext(json_encode($context));
+            $em->persist($panel);
+            $em->flush();
+
+            // @todo refresh panel after edit
+            return new JsonResponse([
+                'data' => ['message' => $this->get('translator')->trans('Panel successfully saved. You can see the changes on next page refresh')]
+            ]);
+        }
+
+        return [
+            'id' => $id,
+            'template' => $panelType->getFormTemplate(),
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * 
+     * @Route("/system/dashboard/sort", name="dt_system_dashboard_sort")
+     * @Method("POST")
+     */
+    public function sortDashboardAction(Request $request) {
+
+        $dashboardPanels = $this->get('dashboard')->getPanels();
+        $panels = array_keys($dashboardPanels);
+
+        $order = $request->request->get('order');
+        $pos = $request->request->get('pos');
+
+        $order = explode(',', $order);
+
+        $order = array_map(function($el){
+            if($part = substr($el, strlen('dashboard-panel-'))) {
+                return $part;
+            };
+        }, $order);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $i = 1;
+        foreach ($order as $name) {
+
+            if($name) {
+
+                $panelConfig =  $em->getRepository('SystemBundle:Panel')
+                ->findOneBy(['user' => $this->getUser(), 'name' => $name]);
+
+                $panelConfig or $panelConfig = new PanelConfig();
+
+                $status = $panelConfig ? $panelConfig->getStatus() : 1;
+
+                $panelConfig->setUser($this->getUser());
+                $panelConfig->setName($name);
+                $panelConfig->setPosition($pos);
+                $panelConfig->setSequence($i++);
+
+                $em->persist($panelConfig);
+                $em->flush();
+            }
+        }
+
+        return new Response();
+    }
+
+    /**
+     * @Route("/system/dashboard/delete", name="dt_system_dashboard_delete")
+     */
+    public function dashboardDeleteAction(Request $request)
+    {
+        // @todo handle csrf token
+        $id = $request->request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $panel = $em->getRepository('SystemBundle:Panel')->find($id);
+        $em->remove($panel);
+        $em->flush();
+
+        return new JsonResponse(['data' => ['status' => 'ok']]);
+    }
+
+    /**
+     * 
+     * @Route("/system/dashboard/toggle_panel", name="dt_system_dashboard_toggle_panel")
+     * @Method("POST")
+     */
+    public function togglePanelAction(Request $request) {
+
+        $name = $request->request->get('panel');
+
+        $em = $this->getDoctrine()->getManager();
+        $panelConfig =  $em->getRepository('SystemBundle:Panel')
+            ->findOneBy(['user' => $this->getUser(), 'name' => $name]);
+
+        $panelConfig or $panelConfig = new PanelConfig();
+
+        $status = $panelConfig->getStatus() ? 0 : 1;
+
+        $panelConfig->setUser($this->getUser());
+        $panelConfig->setStatus($status);
+        $em->persist($panelConfig);
+        $em->flush();
+
+        return new Response();
+    }
+}
