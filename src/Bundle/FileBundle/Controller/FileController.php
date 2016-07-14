c114bd81562b3f3aedf7e6960ee69b24baae9c20
egig <egigundari@gmail.com>
2016-07-14 23:33:53 +0700

    init files

--- src/Bundle/FileBundle/Controller/FileController.php
+++ src/Bundle/FileBundle/Controller/FileController.php
@@ -15,6 +15,7 @@ class FileController extends Controller
     public function indexAction()
     {
         return [
+            'view_id' => 'file',
             'locale' => $this->container->getParameter('locale'),
             'page_title' => $this->get('translator')->trans('File'),
         ];
@@ -33,53 +34,23 @@ class FileController extends Controller
 
     public function dataAction(Request $request)
     {
-        $op = $request->get('op');
-        $path = $request->get('path');
+        $files = []; // @todo
 
-        $res = new JsonResponse();
-
-        try {
+        $filesArr = [];
+        foreach ($files as $file) {
             $data = [];
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
 
-            switch ($op) {
-                case 'ls':
-                    $data = $this->get('dt_file.server')->ls($path);
-                    break;
-                case 'delete':
-                    $data = $this->get('dt_file.server')->delete($path);
-                    break;
-                case 'mkdir':
-                    $folderName = $request->get('folder-name');
-                    $data = $this->get('dt_file.server')->mkdir($path, $folderName);
-                    break;
-                case 'rename':
-                    $newName = $request->get('newName');
-                    $data = $this->get('dt_file.server')->rename($path, $newName);
-                    break;
-                case 'move':
-                    $dest = $request->get('dest');
-                    $data = $this->get('dt_file.server')->move($path, $dest);
-                    break;
-                case 'properties':
-                    $data = $this->get('dt_file.server')->properties($path);
-                break;
-                case 'search':
-                    $q = $request->get('q');
-                    $data = $this->get('dt_file.server')->search($q, $path);
-                break;
-                default:
-                    break;
-            }
-
-            if ($files = $request->files->get('files')) {
-                $data = $this->get('dt_file.server')->upload($path, $files);
-            }
-        } catch (\Exception $e) {
-            $data = ['message' => $e->getMessage(), 'status' => 'error'];
+            $filesArr[] = $data;
         }
 
-        $res->setData($data);
+        $ob = new \StdClass();
+        $ob->data = $filesArr;
+        $ob->recordsTotal = count($filesArr);
+        $ob->recordsFiltered = count($filesArr);
 
-        return $res;
+        return new JsonResponse($ob);
     }
 }
