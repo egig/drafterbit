429aaea8fd6627d0834855f9f05da49b58228959
egig <egigundari@gmail.com>
2016-08-13 14:39:33 +0700

    add some migration and  seeds

---
+++ modules/desk/migrations/20160813140717_init-system.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('system', function (table) {
+      table.increments();
+      table.string('key');
+      table.string('val');
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('system')
+};
