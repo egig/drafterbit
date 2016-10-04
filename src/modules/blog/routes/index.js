17736783589e7f50634b3d6e25fc6285ef0ee9b5
egig <egigundari@gmail.com>
2016-10-04 12:36:57 +0700

    clean up

--- src/modules/blog/routes/index.js
+++ src/modules/blog/routes/index.js
@@ -3,12 +3,10 @@ var router = express.Router();
 
 var post = require('./post');
 var category = require('./category');
-var comment = require('./comment');
 var tag = require('./tag');
 
 router.use('/blog/post', post);
 router.use('/blog/category', category);
-router.use('/blog/comment', comment);
 router.use('/blog/tag', tag);
 
 module.exports = router;
