c9502fae4da8bf3591dd93596d884788c2f1e67f
Egi Gundari <egi.gundari@qraved.com>
2018-02-06 03:15:13 +0700

    better dashboard

--- src/common/modules/common/components/Layout.js
+++ src/common/modules/common/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Nav from '../../common/components/Nav';
 import withStyle from '../../../withStyle';
 import Style from './Layout.style';
+import { Link } from 'react-router-dom';
 
 class Layout extends React.Component {
 	render() {
@@ -10,10 +10,74 @@ class Layout extends React.Component {
 
 		return (
 			<span>
-				<Nav/>
-				<div className={`container ${classNames.layoutContainer}`}>
-					{this.props.children}
-	      </div>
+				<nav className={`${classNames.navbar} navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0`}>
+		      <a className={`${classNames.navbarBrand} navbar-brand col-sm-3 col-md-2 mr-0`} href="#">drafterbit</a>
+		      <ul className="navbar-nav px-3">
+		        <li className="nav-item text-nowrap">
+		          <a className="nav-link" href="#">Sign out</a>
+		        </li>
+		      </ul>
+		    </nav>
+				<div className="container-fluid">
+		      <div className="row">
+		        <nav className={`col-md-2 d-none d-md-block bg-light ${classNames.sidebar}`}>
+		          <div className={classNames.sidebarSticky}>
+		            <ul className="nav flex-column">
+		              <li className="nav-item">
+		                <Link className="nav-link active" to="/">
+		                  <span data-feather="home"></span>
+		                  Dashboard <span className="sr-only">(current)</span>
+		                </Link>
+		              </li>
+		              <li className="nav-item">
+		                <Link className="nav-link" to="/projects">
+		                  <span data-feather="shopping-cart"></span>
+		                  Projects
+		                </Link>
+		              </li>
+		            </ul>
+
+		            {/*<h6 className={`${classNames.sidebarHeading} d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted`}>*/}
+		              {/*<span>Saved reports</span>*/}
+		              {/*<a className="d-flex align-items-center text-muted" href="#">*/}
+		                {/*<span data-feather="plus-circle"></span>*/}
+		              {/*</a>*/}
+		            {/*</h6>*/}
+		            {/*<ul className="nav flex-column mb-2">*/}
+		              {/*<li className="nav-item">*/}
+		                {/*<a className="nav-link" href="#">*/}
+		                  {/*<span data-feather="file-text"></span>*/}
+		                  {/*Current month*/}
+		                {/*</a>*/}
+		              {/*</li>*/}
+		              {/*<li className="nav-item">*/}
+		                {/*<a className="nav-link" href="#">*/}
+		                  {/*<span data-feather="file-text"></span>*/}
+		                  {/*Last quarter*/}
+		                {/*</a>*/}
+		              {/*</li>*/}
+		              {/*<li className="nav-item">*/}
+		                {/*<a className="nav-link" href="#">*/}
+		                  {/*<span data-feather="file-text"></span>*/}
+		                  {/*Social engagement*/}
+		                {/*</a>*/}
+		              {/*</li>*/}
+		              {/*<li className="nav-item">*/}
+		                {/*<a className="nav-link" href="#">*/}
+		                  {/*<span data-feather="file-text"></span>*/}
+		                  {/*Year-end sale*/}
+		                {/*</a>*/}
+		              {/*</li>*/}
+		            {/*</ul>*/}
+		          </div>
+		        </nav>
+
+		        <main role="main" className={`col-md-9 ml-sm-auto col-lg-10 pt-3 px-4`}>
+		          <h2>Section title</h2>
+			        {this.props.children}
+		        </main>
+		      </div>
+		    </div>
 			</span>
 		);
 	}
--- src/common/modules/common/components/Layout.style.js
+++ src/common/modules/common/components/Layout.style.js
@@ -1,5 +1,62 @@
 module.exports = {
-	layoutContainer: {
-		marginTop: "20px"
+
+	/**
+	 * Sidebar
+	 */
+	sidebar: {
+		position: "fixed",
+		top: 0,
+		bottom: 0,
+		left: 0,
+		zIndex: 100, /* Behind the navbar */
+		padding: 0,
+		boxShadow: "inset -1px 0 0 rgba(0, 0, 0, .1)"
+	},
+
+	sidebarSticky: {
+		position: "sticky",
+		top: "48px", /* Height of navbar */
+		height: "calc(100vh - 48px)",
+		paddingTop: ".5rem",
+		overflowX: "hidden",
+		overflowY: "auto" /* Scrollable contents if viewport is shorter than content. */
+	},
+
+
+	sidebarNavLink: {
+		fontWeight: 500,
+		color: "#333",
+	},
+
+	sidebarHeading: {
+		fontSize: ".75rem",
+		textTransform: "uppercase",
+	},
+
+	/*
+	 * Navbar
+	 */
+	navbar: {
+		zIndex: 101
+	},
+	navbarBrand: {
+		paddingTop: ".75rem",
+		paddingBottom: ".75rem",
+		fontSize: "1rem",
+		backgroundColor: "rgba(0, 0, 0, .25)",
+		boxShadow: "inset -1px 0 0 rgba(0, 0, 0, .25)"
 	}
-};
+
+}
+
+// .form-control-dark:focus {
+// 	border-color: transparent;
+// 	box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
+// }
+
+/*
+ * Utilities
+ */
+
+// .border-top { border-top: 1px solid #e5e5e5; }
+// .border-bottom { border-bottom: 1px solid #e5e5e5; }
