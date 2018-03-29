02c5c06c4545f07edbf83c5653181da1daf377d4
Egi Gundari <egi.gundari@qraved.com>
2018-03-29 21:15:05 +0700

    add state

--- src/common/modules/content/components/Contents.js
+++ src/common/modules/content/components/Contents.js
@@ -40,19 +40,7 @@ class Contents extends React.Component {
 				Header: '#ID',
 				accessor: 'id' // String-based value accessors!
 			}
-			// , {
-			// 	Header: 'Age',
-			// 	accessor: 'age',
-			// 	Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-			// }, {
-			// 	id: 'friendName', // Required because our accessor is not a string
-			// 	Header: 'Friend Name',
-			// 	accessor: d => d.friend.name // Custom value accessors!
-			// }, {
-			// 	Header: props => <span>Friend Age</span>, // Custom header components!
-			// 	accessor: 'friend.age'
-			// }
-			];
+		];
 
 		this.props.ctFields.fields.map(f => {
 			columns.push({
--- src/common/modules/project/components/ProjectDashboard.js
+++ src/common/modules/project/components/ProjectDashboard.js
@@ -2,19 +2,48 @@ import React from 'react';
 import ProjectLayout from './ProjectLayout';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import drafterbit from 'drafterbit';
+
+
 class ProjectDashboard extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			contentTypeStat: []
+		}
+	}
+
+	componentDidMount() {
+		let client  = drafterbit.createClient({});
+		client.getProjectStat(this.props.match.params.project_id)
+			.then(r => {
+				this.setState({
+					contentTypeStat: r.content_type
+				});
+			})
+	}
 
 	render() {
 
 		return (
 			<ProjectLayout title="Project Dashboard">
-				<div className="row justify-content-center">
 					{!this.props.project.content_types.length &&
+					<div className="row justify-content-center">
 						<div className="col-6">
 							<p>There is no content type yet :(</p>
 							<Link to={`/project/${this.props.project.id}/content_types/new`} className="btn btn-success">Create Content Type</Link>
 						</div>
+					</div>
 					}
+
+				<div className="row">
+					<div>
+							{this.state.contentTypeStat.map((item,i) => {
+								return <p key={i}>{item.name } : {item.content_count}</p>
+							})}
+						</div>
 				</div>
 			</ProjectLayout>
 		);
