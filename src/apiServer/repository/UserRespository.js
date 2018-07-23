65e03ce6ffa0871323f8acf60e95a90afe67d20d
Egi Gundari <egi.gundari@qraved.com>
2018-07-23 10:00:08 +0700

    crud user use mongo

--- src/apiServer/repository/UserRespository.js
+++ src/apiServer/repository/UserRespository.js
@@ -4,21 +4,17 @@ import mongoose from 'mongoose';
 import userSchema from '../../schema/userSchema'
 
 
-
 class UserRespository extends BaseRespository {
 
     getUsers() {
         return new Promise((resolve, reject) => {
 
-            this.connection.connect();
-
-            this.connection.query('SELECT id,email,first_name,last_name from users', function (error, results, fields) {
-                if (error) return reject(error);
-                return resolve(results);
-            });
-
-            this.connection.end();
-
+        	// TODO dont create model each time
+	        const User = mongoose.model('User', userSchema);
+	        User.find({}, function(err, users) {
+		        if (err) return reject(err);
+		        return resolve(users);
+	        });
         });
     }
 
@@ -58,7 +54,7 @@ class UserRespository extends BaseRespository {
 		        password: password
 	        });
 
-	        newUser.save(function (err, fluffy) {
+	        newUser.save(function (err, user) {
 		        if (err) return reject(err);
 		        resolve(true)
 	        });
@@ -74,17 +70,11 @@ class UserRespository extends BaseRespository {
     deleteUser(userId) {
         return new Promise((resolve, reject) => {
 
-            this.connection.connect();
-
-            this.connection.query('DELETE FROM users WHERE id=?',
-                [userId],
-                (error, results, fields) => {
-                    if (error) return reject(error);
-                    return resolve(results);
-                });
-
-            this.connection.end();
-
+	        const User = mongoose.model('User', userSchema);
+	        User.find({ _id:userId }).remove((err, results) => {
+		        if (err) return reject(err);
+		        return resolve(results);
+	        });
         });
     }
 
@@ -99,18 +89,17 @@ class UserRespository extends BaseRespository {
     updateUser(userId, firstName, lastName, email) {
         return new Promise((resolve, reject) => {
 
-            console.log(userId);
+	        const User = mongoose.model('User', userSchema);
 
-            this.connection.connect();
-
-            this.connection.query('UPDATE users SET first_name=?,last_name=?,email=? WHERE id=?',
-                [firstName,lastName,email,userId],
-                (error, results, fields) => {
-                    if (error) return reject(error);
-                    return resolve(results);
-                });
-
-            this.connection.end();
+	        User.update({ _id: userId }, { $set:{
+		        first_name: firstName,
+		        last_name: lastName,
+		        email: email
+	        }
+	        }, (err) => {
+		        if (err) return reject(err);
+		        return resolve(true);
+	        });
 
         });
     }
