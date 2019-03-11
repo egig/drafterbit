d3adb605ed46a4c0a1b4500b8030d1d3a5828377
Egi Gundari <egi.gundari@qraved.com>
2019-03-12 00:34:31 +0700

    swagger wip

--- src/server/routes/swagger.js
+++ src/server/routes/swagger.js
@@ -2,57 +2,78 @@ const express = require('express');
 let router = express.Router();
 const ContentTypeRepository = require('../repository/ContentTypeRepository');
 
-let getSwaggerSpec = function () {
-	return new Promise((resolve, reject) => {
-
-		let swaggerSpec = {
-			"swagger": "2.0",
-			"info": {
-				"title": "",
-				"description": "",
-				"version": "1.0"
-			},
-			"produces": ["application/json"],
-			"host": "localhost:8000",
-			"basePath": "/api/swagger/v1",
-			"paths": {
-				"/test1": {
-					"get": {
-						"x-swagger-router-controller": "middleware-name1",
-						"operationId": "swagTest",
-						"tags": ["/test"],
-						"description": "",
-						"parameters": [],
-						"responses": {}
-					}
-				}
-			}
-		}
-
-		resolve(swaggerSpec);
-	})
-};
-
-
-app.get("/_swagger_spec.json",  function (req, res) {
+router.get("/_swagger_spec.json",  function (req, res) {
 
 	(async function () {
 
 		try {
 			let r = new ContentTypeRepository(req.app);
 			let results = await r.getContentTypes();
-			res.send(results);
+
+			let swaggerSpec = {
+				"swagger": "2.0",
+				"info": {
+					"title": "",
+					"description": "",
+					"version": "1.0"
+				},
+				"produces": ["application/json"],
+				"host": "localhost:8000",
+				"basePath": "/",
+			}
+
+			let paths = {};
+			results.map((r) => {
+
+				let parameters = r.fields.map(f => {
+					return {
+						in: "body",
+						name: f.name,
+						type: "string",
+						description: f.label
+					}
+				});
+
+				paths[`/${r.slug}`] = {
+					"get": {
+						"tags": [`/${r.slug}`],
+						"description": "",
+						"parameters": [],
+						"responses": {}
+					},
+					"post": {
+						"tags": [`/${r.slug}`],
+						"description": "",
+						"parameters": parameters,
+						"responses": {}
+					}
+				}
+
+				paths[`/${r.slug}/{${r.slug}_id}`] = {
+					"patch": {
+						"tags": [`/${r.slug}`],
+						"description": "",
+						"parameters": parameters,
+						"responses": {}
+					},
+					"delete": {
+						"tags": [`/${r.slug}`],
+						"description": "",
+						"parameters": [],
+						"responses": {}
+					}
+				}
+			});
+
+			swaggerSpec.paths = paths;
+
+			res.send(swaggerSpec);
 		} catch (e ) {
 			res.status(500);
 			res.send(e.message);
 		}
 
 	})();
-
-	getSwaggerSpec()
-		.then(r => {
-			res.send(r);
-		})
 });
 
 
